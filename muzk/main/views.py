373c7d036cdebd626fd984ba92ced369b0f78771
YarBurArt <yaroslav.chayk@gmail.com>
2023-01-24 20:09:58 +0500

    add about info

--- muzk/main/views.py
+++ muzk/main/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 
 
+about_me_str = """
+I am a passionate learner with a love of programming. 
+I have recently started learning Python and C sharp, 
+and am excited to further my knowledge in these languages. 
+As a beginner, I am focusing on the fundamentals of programming 
+and gaining a strong understanding of the basic concepts. 
+I am also exploring the various libraries, frameworks, and tools available in both languages.
+In addition to programming, I am also interested in digital art. 
+I enjoy using various digital art tools to create unique designs and illustrations. 
+I love the challenge of creating something new, and the freedom that comes with digital art. 
+I am constantly experimenting with various techniques and tools, and am excited to see where my creativity takes me.
+I believe that programming and digital art complement each other. 
+By understanding the basics of programming, I am able to create more complex and interesting artwork. 
+Similarly, my knowledge of digital art makes me a better programmer, 
+allowing me to create more visually appealing applications and websites.
+My passion for programming and digital art has led me to develop a strong work ethic, determination, 
+and motivation to succeed. I am constantly pushing myself to learn more and become better at both disciplines. 
+I am excited to see how far I can take my programming and digital art skills.
+"""
+
+
 def index(request):
     return render(request, "main/index.html")
 
 
 def about(request):
-    return render(request, "main/about.html")
+    return render(request, "main/about.html", context={'text': about_me_str})
 
 
 def contact(request):
