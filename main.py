5f9017ec76ba4361ad29d7695ac87ef3bf5ae646
YarBurArt <81087967+YarBurArt@users.noreply.github.com>
2023-01-27 19:09:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os 
 
 
-def start_project(name):
-    print(f'Hi, {name} huilo')  
-    print(f"Now I'm going to run a project for you, mr. {name} huilo")
-    os.system("cd muzk && python manage.py runserver")
+def start_project(dev_name, proj_name):
+    print(f'Hi, {dev_name} huilo')  
+    print(f"Now I'm going to run a project for you, mr. {dev_name} huilo")
+    os.system(f"cd {proj_name} && python manage.py runserver")
     
     
 if __name__ == '__main__':
-    start_project('YarBurArt')
+    start_project('YarBurArt', 'muzk')
