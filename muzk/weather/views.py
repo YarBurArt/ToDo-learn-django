51549202df523d04d39c6fc6a20b7d4fcb0ed145
YarBurArt <yaroslav.chayk@gmail.com>
2023-04-17 19:01:30 +0500

    clean code

--- muzk/weather/views.py
+++ muzk/weather/views.py
@@ -11,21 +11,22 @@ environ.Env.read_env()
 
 
 def index(request):
-    return render(request, "weather/choice.html")
+    return render(request, "weather/choice.html", 
+                  context={
+                    "city": '',
+                    "temp": .1
+                  })
 
 
 def get_weather(request, city):
-    api_key = os.getenv('WEATHERAPI') # debug it
-
+    api_key = os.getenv('WEATHERAPI')
 
     res = requests.get("http://api.openweathermap.org/data/2.5/find",
                        params={'q': city, 'type': 'like',
                                'units': 'metric', 'APPID': api_key})
     data = res.json()
     temp = data['list'][0]['main']['temp']
-    # return HttpResponse(f"Weather for {city} \n "
-    #                     f"temp: {temp}")
-    return render(request, "weather/weather.html",
+    return render(request, "weather/choice.html",
                   context={
                       "city": city,
                       "temp": temp
