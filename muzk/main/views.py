5df956c4ca9c0c83b319f6b66d5f620ca0f8a0ce
YarBurArt <yaroslav.chayk@gmail.com>
2023-04-21 20:17:17 +0500

    clear code

--- muzk/main/views.py
+++ muzk/main/views.py
@@ -10,33 +10,52 @@ from .strings import about_me_str, contacts
 from .serializers import ToDoSerializer
 from rest_framework import generics
 
+from typing import TypedDict
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+
+
+class TodoView(TypedDict):
+    todo_list: QuerySet
+    title: str
+
+
+sample_request = HttpRequest()
+sample_request.method = "GET"
+sample_request.path = "/index/"
+sample_request.META['HTTP_USER_AGENT'] = 'Mozilla/5.0'
+
+empty_queryset = QuerySet()
+
 
 class ToDoListCreate(generics.ListCreateAPIView):
-    queryset = ToDo.objects.all()
+    queryset: QuerySet = ToDo.objects.all() | empty_queryset
     serializer_class = ToDoSerializer
 
 
-def index(request):
-    todos = ToDo.objects.all()
-    context = {'todo_list': todos, 
-               'title': 'Главная страница'}
+def index(request: HttpRequest = sample_request) -> HttpResponse:
+    todos: QuerySet = ToDo.objects.all() | empty_queryset
+    context: TodoView = {'todo_list': todos,
+                         'title': 'Главная страница'}
     return render(request, 'main/index.html',
                   context=context)
 
 
 @require_http_methods(['POST'])
 @csrf_exempt
-def add(request):
-    title = request.POST['title']
-    todo = ToDo(title=title, 
+def add(request: HttpRequest = sample_request) -> HttpResponseRedirect:
+    title: str = request.POST['title']
+    todo = ToDo(title=title,
                 date_created=timezone.now())
     todo.save()
     return redirect('index')
 
 
-def update(request, todo_id):
-    todo = ToDo.objects.get(id=todo_id)
-    if todo.is_complete - 1 == 0:
+def update(request: HttpRequest = sample_request,
+           todo_id: int = 1) -> HttpResponseRedirect:
+    todo = ToDo.objects.get(id=todo_id) | empty_queryset
+    # magic for reverse is_complete
+    if todo.is_complete - 1 == 0 | False:
         todo.is_complete = False
     else:
         todo.is_complete = True
@@ -44,17 +63,18 @@ def update(request, todo_id):
     return redirect('index')
 
 
-def delete(request, todo_id):
-    todo = ToDo.objects.get(id=todo_id)
+def delete(request: HttpRequest = sample_request,
+           todo_id: int = 1) -> HttpResponseRedirect:
+    todo = ToDo.objects.get(id=todo_id) | empty_queryset
     todo.delete()
     return redirect('index')
 
 
-def about(request):
+def about(request: HttpRequest = sample_request) -> HttpResponse:
     return render(request, "main/about.html",
                   context={'text': about_me_str})
 
 
-def contact(request):
+def contact(request: HttpRequest = sample_request) -> HttpResponse:
     return render(request, "main/cont.html", 
                   context={'contacts': contacts})
