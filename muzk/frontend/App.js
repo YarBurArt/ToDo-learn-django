0bf2ddddacf7d07aa5130e2a175c6bfec0b3efaa
YarBurArt <81087967+YarBurArt@users.noreply.github.com>
2024-05-13 19:01:45 +0500

    clean frontend

--- muzk/frontend/App.js
+++ muzk/frontend/App.js
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm";
 
+import {content_about, contacts, 
+        dropdown_uris, baseUrl} from "./constants";
+
 // all in one file because it's more interesting than react as a separate server
 // so you can see the combination of react and jinja2
 // although it's still bad code, I'm just writing this for fun.
@@ -21,52 +24,28 @@ function formatDate(dateString) {
 }
 const UpdateTodoButton = ({ todoId }) => { // TODO:
   return (
-    <a href={`http://127.0.0.1:8000/update_todo/${todoId}`}>
+    <a href={`${baseUrl}update_todo/${todoId}`}>
       <button className="update-todo-button">Update status</button>
     </a>
   );
 };
 const DeleteTodoButton = ({ todoId }) => { // TODO:
   return (
-    <a href={`http://127.0.0.1:8000/delete_todo/${todoId}`}>
+    <a href={`${baseUrl}delete_todo/${todoId}`}>
       <button className="delete-todo-button">Delete</button>
     </a>
   )
 };
 const AboutBlock = () => {
-  let content = `
-## About Me
-
-I'm a passionate learner with a love for programming!  I recently began diving into the worlds of Python and C#, eager to expand my knowledge in these languages. As a beginner, I'm focusing on building a strong foundation by mastering the fundamentals of programming concepts. I'm also exploring the vast landscape of libraries, frameworks, and tools available for both languages.
-
-- Self-Driven Learner
-
-Beyond programming, I'm also captivated by the world of digital art.  I find joy in using various digital art tools to bring unique designs and illustrations to life. The challenge of creating something new and the boundless freedom of digital art truly inspire me. I'm constantly experimenting with different techniques and tools, eager to see where my creative journey takes me.
-
-- Strong Work Ethic and Determination
-
-In my eyes, programming and digital art beautifully complement each other. By grasping programming fundamentals, I'm able to craft more intricate and captivating artwork. Likewise, my understanding of digital art empowers me as a programmer, allowing me to design visually stunning applications and websites.
-
-- Cross-disciplinary Skills
-
-My passion for both programming and digital art has fueled a strong work ethic, determination, and unwavering motivation to succeed. I constantly push myself to learn more and refine my skills in both disciplines. I'm incredibly excited to see how far I can take my programming and digital art journey!
-`;
   return (
     <div className="mrkdn" id="about">
     <ReactMarkdown remarkPlugins={[remarkGfm]}>
-      {content}
+      {content_about}
     </ReactMarkdown>
     </div>
   )
 }
 const Contact = () => {
-  let contacts = [
-    {'url': 'https://github.com/yarburart',
-    'name': 'Email', 'add_i': 'send message if you have questions'},
-    {'url': 'https://github.com/yarburart',
-    'name': 'GitHub', 'add_i': 'some programming progects'},
-    {'url': 'https://www.artstation.com/yaburart',
-    'name': 'Artstation', 'add_i': 'digital programming'}]
   return (
     <div className="contact" id="contact">
         {contacts.map((contact, index) => (
@@ -86,32 +65,17 @@ const Dropdown = () => {
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
-
+  
   return (
     <div className="dropdown">
-      <button className="dropdown-button" onClick={toggleDropdown}>
-        Menu
-      </button>
+      <button className="dropdown-button" onClick={toggleDropdown}>Menu</button>
       {isOpen && (
         <ul className="dropdown-content task_ul">
-          <li className="task_li">
-            <a href="#top" onClick={toggleDropdown}>Home</a>
-          </li>
-          <li className="task_li">
-            <a href="#about" onClick={toggleDropdown}>About</a>
-          </li>
-          <li className="task_li">
-            <a href="#contact" onClick={toggleDropdown}>Contact</a>
-          </li>
-          <li className="task_li">
-            <a href="/blog" onClick={toggleDropdown}>Blog</a>
-          </li>
-          <li className="task_li">
-            <a href="/new-post" onClick={toggleDropdown}>New Post</a>
-          </li>
-          <li className="task_li">
-            <a href="/login" onClick={toggleDropdown}>Login</a>
-          </li>
+          {dropdown_uris.map((elem_uri, index) => (
+            <li className="task_li">
+              <a href={elem_uri.uri} onClick={toggleDropdown}>{elem_uri.name}</a>
+            </li>
+          ))}
         </ul>
       )}
     </div>
@@ -128,7 +92,7 @@ class App extends Component {
   }
   // get tasks 
   componentDidMount() {
-    fetch('http://127.0.0.1:8000/api/todo') // TODO: clean front
+    fetch(`${baseUrl}api/todo`) // TODO: clean front
       .then(response => {
         if (response.status > 400) {
           return this.setState(() => {
@@ -140,10 +104,7 @@ class App extends Component {
         data => {
         this.setState(() => {
           console.log(data);
-          return {
-            data,
-            loaded: true
-          };
+          return { data, loaded: true };
         });
       });
   }
---
+++ muzk/frontend/constants.js
@@ -0,0 +1,33 @@
+export const baseUrl = "http://127.0.0.1:8000/";
+export const content_about = `
+## About Me
+
+I'm a passionate learner with a love for programming!  I recently began diving into the worlds of Python and C#, eager to expand my knowledge in these languages. As a beginner, I'm focusing on building a strong foundation by mastering the fundamentals of programming concepts. I'm also exploring the vast landscape of libraries, frameworks, and tools available for both languages.
+
+- Self-Driven Learner
+
+Beyond programming, I'm also captivated by the world of digital art.  I find joy in using various digital art tools to bring unique designs and illustrations to life. The challenge of creating something new and the boundless freedom of digital art truly inspire me. I'm constantly experimenting with different techniques and tools, eager to see where my creative journey takes me.
+
+- Strong Work Ethic and Determination
+
+In my eyes, programming and digital art beautifully complement each other. By grasping programming fundamentals, I'm able to craft more intricate and captivating artwork. Likewise, my understanding of digital art empowers me as a programmer, allowing me to design visually stunning applications and websites.
+
+- Cross-disciplinary Skills
+
+My passion for both programming and digital art has fueled a strong work ethic, determination, and unwavering motivation to succeed. I constantly push myself to learn more and refine my skills in both disciplines. I'm incredibly excited to see how far I can take my programming and digital art journey!
+`;
+export const contacts = [
+    {'url': 'https://github.com/yarburart',
+    'name': 'Email', 'add_i': 'send message if you have questions'},
+    {'url': 'https://github.com/yarburart',
+    'name': 'GitHub', 'add_i': 'some programming progects'},
+    {'url': 'https://www.artstation.com/yaburart',
+    'name': 'Artstation', 'add_i': 'digital programming'}];
+export const dropdown_uris = [
+    {'name':'Home', 'uri':'#top'},
+    {'name':'About', 'uri':'#about'},
+    {'name':'Contact', 'uri':'#contact'},
+    {'name':'Blog', 'uri':'/blog'},
+    {'name':'New post', 'uri':'/new-post'},
+    {'name':'Login', 'uri':'/login'},
+  ];
