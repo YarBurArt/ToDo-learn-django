32b1a450028e0e07b762017c807997e1c7d596f3
YarBurArt <81087967+YarBurArt@users.noreply.github.com>
2025-12-14 14:31:21 +0500

    upd django, fix auth

--- muzk/muzk/settings.py
+++ muzk/muzk/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'blog',
     'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
